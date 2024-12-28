047e8fc7c5a744e344ee8e2f4e7948aad0a03eb2
taumo <taumoh@mail.ru>
2024-12-28 10:50:05 +0700

    again commit

--- tests/test_button.py
+++ tests/test_button.py
@@ -17,10 +17,10 @@ def test_button_exist(browser):
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
 
 
-# def test_button_exist_2(browser):
-#     browser.get('https://www.qa-practice.com/elements/button/like_a_button')
-#     assert browser.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
+def test_button_exist_2(browser):
+    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
+    assert browser.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
 
 
-# def test_me():
-#     assert 1 == 1
+def test_me():
+    assert 1 == 1
